fb09cb741d9652997eeddf5625a539e2a235d11e
BaconErie <baconerie0001@gmail.com>
2022-11-22 15:02:19 +0000

    Connect app urls

--- src/two_n_two/urls.py
+++ src/two_n_two/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('study/', include(study.urls)),
+    path('auth/', include(auth.urls)),
+    path('user/', include(user.urls))
 ]
