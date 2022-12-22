45f82bef048ba9fa0cef0fca281a656df29686f1
BaconErie <baconerie0001@gmail.com>
2022-12-22 21:30:45 +0000

    add ok_after attribute

--- src/user/models.py
+++ src/user/models.py
@@ -4,3 +4,4 @@ class User(models.Model):
     username = models.CharField(max_length=20)
     hashed_password = models.TextField()
     date_created = models.DateTimeField()
+    ok_after = models.DateTimeField() # Any token created BEFORE this date is INVALID
