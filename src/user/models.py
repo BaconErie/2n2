df755f148c0d044bdf99f0a145d28d955aefe89c
BaconErie <baconerie0001@gmail.com>
2022-12-22 21:10:07 +0000

    Don't need salt attribute; its built into the hash

--- src/user/models.py
+++ src/user/models.py
@@ -3,5 +3,4 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20)
     hashed_password = models.TextField()
-    salt = models.TextField()
     date_created = models.DateTimeField()
