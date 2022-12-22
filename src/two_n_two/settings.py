723094f800b2fe760103356f7f92a738498f36c5
BaconErie <baconerie0001@gmail.com>
2022-12-22 02:13:51 +0000

    we need these or else break

--- src/two_n_two/settings.py
+++ src/two_n_two/settings.py
@@ -31,8 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # bruh why do i need to have these
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+
     'study.apps.StudyConfig',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
