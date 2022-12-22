5eb5ebef4553adc3aac3a4d825d5dc7c5674de83
BaconErie <baconerie0001@gmail.com>
2022-12-22 01:45:41 +0000

    Classes for Q&A questions

--- src/study/models.py
+++ src/study/models.py
@@ -6,3 +6,16 @@ class StudyGuide(models.Model):
     date_created = models.DateTimeField()
     last_updated = models.DateTimeField()
     public = models.BooleanField(default=False)
+
+class QAReference(models.Model):
+    # This class keeps track of the many questions and answers a Q&A question may have
+    # We can't just use a many to many relationship because we want to know which questions are part of the same Q&A Question
+    study_guide = models.ForeignKey(StudyGuide, on_delete=models.CASCADE)
+
+class QAQuestion(models.Model):
+    content = models.TextField()
+    reference_qa = models.ForeignKey(QAReference, on_delete=CASCADE)
+
+class QAAnswer(models.Model):
+    content = models.TextField()
+    reference_qa = models.ForeignKey(QAReference, on_delete=CASCADE)
